ff7bc030ce668490f56ab78c5b8a7e669e4ff417
Jeff Minard <chuyskywalker@users.noreply.github.com>
2023-06-20 20:35:00 -0700

    Create config.h

---
+++ config.h
@@ -0,0 +1,9 @@
+
+// Your GPRS credentials, if any
+const char apn[] = "xxx";
+const char gprsUser[] = "xxx";
+const char gprsPass[] = "xxx";
+
+// the traccar server you'll submit to
+const char traccar_server[] = "1.2.3.4";
+const int traccar_port = 5055; // osmand port, see https://www.traccar.org/osmand/
